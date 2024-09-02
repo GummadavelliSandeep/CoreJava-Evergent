72ccb48166135fcac30e0a0112423c46d2b35720
Sandeep Gummadavelli <Sandeep.Gummadavelli@etplin.com>
2024-09-02 22:36:19 +0530

    Updating Application files

--- ETPL714-G_SANDEEP Core-Java-Applications/ETPL14 Case Study-2-Customer-Support-Ticketiing-Sysetm/SupportTicketSystemInterface.java
+++
@@ -1,9 +0,0 @@
-package Package.com.evergent.coreJava.collections.sandeep.customersupportingticketingsystem;
-
-public interface SupportTicketSystemInterface {
-	public void addTicket(SupportTicketSystemBean sb );
-	public void searchByTicketId(int ticketId);
-	public void processNextTicket();
-	public void peekNextTicket();
-	public void displayQueue();
-}
