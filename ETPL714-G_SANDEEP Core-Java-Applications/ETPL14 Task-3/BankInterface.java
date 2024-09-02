3aa9af6279a66dec3e511b4c37dbb079f43fa917
Sandeep Gummadavelli <Sandeep.Gummadavelli@etplin.com>
2024-09-02 22:40:03 +0530

    Adding Task-3 Application

---
+++ ETPL714-G_SANDEEP Core-Java-Applications/ETPL14 Task-3/BankInterface.java
@@ -0,0 +1,8 @@
+package Package.com.evergent.coreJava.collections.sandeep.task3;
+
+public interface BankInterface {
+
+	public void addRegistration(BankBean b);
+	public void SearchByCustomerId(int customerId);
+	public void displayAll();
+}
