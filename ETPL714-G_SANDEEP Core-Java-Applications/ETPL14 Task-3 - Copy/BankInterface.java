38a60155fc8a7a3f79605d12e9f306f102e8ecee
Sandeep Gummadavelli <Sandeep.Gummadavelli@etplin.com>
2024-09-22 15:04:30 +0530

    CoreJava-Applications

--- ETPL714-G_SANDEEP Core-Java-Applications/ETPL14 Task-3 - Copy/BankInterface.java
+++
@@ -1,8 +0,0 @@
-package Package.com.evergent.coreJava.collections.sandeep.task3;
-
-public interface BankInterface {
-
-	public void addRegistration(BankBean b);
-	public void SearchByCustomerId(int customerId);
-	public void displayAll();
-}
